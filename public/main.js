1bc11d6b519d4b6f010b304d1f60a44855a81909
TenzinNsut <tenzin.lekshey.ug21@nsut.ac.in>
2022-11-14 18:55:56 +0530

    background movement changed

--- public/main.js
+++ public/main.js
@@ -33,8 +33,8 @@ function parallax(e) {
     document.querySelectorAll(".object").forEach(function (move) {
         
         var moving_value = move.getAttribute("data-value");
-        var x = (e.clientX * moving_value) / 200;
-        var y = (e.clientY * moving_value) /200;
+        var x = (e.clientX * moving_value) / 100;
+        var y = (e.clientY * moving_value) /100;
 
         move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
 
